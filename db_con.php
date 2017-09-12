04aeb310f4fb9e6be84798e9d3c074a525d186f1
carolineheideskov <caroline.heideskov@gmail.com>
2017-09-12 22:48:03 +0200

    Webshop

--- db_con.php
+++ db_con.php
@@ -1,18 +1,34 @@
 <?php 
+
 // forbindelse til mySQL serveren ved brug af mysqli metode
-// 1. Variabler (konstanter) til forbindelsen
-const HOSTNAME = 'carolineheideskov.dk.mysql'; // server
+
+// 1. variabler (konstanter) til forbindelsen
+
+/* const HOSTNAME = 'carolineheideskov.dk.mysql'; // server
 const MYSQLUSER = 'carolineheideskov_dk'; // superbruger
 const MYSQLPASS = '3j3K8wSZ'; // password
-const MYSQLDB = 'carolineheideskov_dk'; // database navn
+const MYSQLDB = 'carolineheideskov_dk'; // database navn */
+
+const HOSTNAME = 'localhost'; // server
+const MYSQLUSER = 'root'; // superbruger
+const MYSQLPASS = 'root'; // password
+const MYSQLDB = 'webshop1'; // database navn
+
 // 2. Forbindelsen via mysqli metoden
+
 $con = new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS, MYSQLDB);
-// at sikre sig, at alle utf8-tegn kan blive brugt under forbindelsen
+
+// at sikre sig, at alle utf 8 tegn kan blive brugt i forbindelsen
 $con->set_charset ('utf8');
-// 3. Tjek
-// hvis der er fejl i forbindelsen
+
+
+// 3. Tjek om forbindelse
+
+// hvis der er fejl i forbindelsen 
 if($con->connect_error){
 	die($con->connect_error);
-// hvis der er hul i gennem
+//hvis der er hul i gennem
+} else {
+	
 }
-// PHP slut tag kan undlades, hvis filen indeholder "rent" PHP
+// php slut tag kan undlades, hvis filen indeholder rent php 
---
+++ success.php
@@ -0,0 +1,73 @@
+<?php session_start(); ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Success</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+<body>
+
+<h2>Din ordre er modtaget!</h2>
+
+
+<div class="container" style="border-width: 1px;border-style: solid;border-color:black;">
+<h4>Kvittering for køb</h4>
+<hr>
+<?php
+
+require_once('db_con.php');
+	
+	// henter order_id fra vores session 
+$order_id = $_SESSION['order_id'];
+	
+$sql = 
+	'SELECT product.id, product.name, product.price, order_items.order_id, order_items.product_id, order_items.quantity
+	FROM order_items, product 
+	WHERE product.id = order_items.product_id AND '.$order_id.' = order_items.order_id';
+	
+$stmt = $con->prepare($sql);
+$stmt->execute();
+$stmt->bind_result($pid, $name, $price, $oioid, $oipid, $quantity);
+	
+while ($stmt->fetch()){
+		echo 'Produkt: ';
+		echo $name ;
+		
+		echo '<br> Antal: ';
+		echo $quantity ;
+
+	echo '<br> Pris: ';
+		echo $price;
+		echo ' DKK';
+		echo '<hr>';
+	
+		
+}
+?>
+
+<?php 
+	
+	// ganger antal og pris
+	$sql = 'SELECT sum(order_items.quantity * product.price)
+			FROM order_items, product WHERE product.id = order_items.product_id AND '.$order_id.' = order_items.order_id';
+	
+	$stmt = $con->prepare($sql);
+	$stmt->execute();
+	$stmt->bind_result($total);
+	
+while ($stmt->fetch()){
+	echo 'Totalpris : ';
+	echo $total;
+	echo ' DKK';
+	echo '<br /><br />';
+}
+	?>
+
+<a href="index.php"><button>Tilbage til startside</button></a>
+<br><br>
+</div>
+</body>
+</html>
